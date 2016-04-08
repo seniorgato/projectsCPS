e97f06a6015add20354c9e6a9382c6db288948c1
seniorgato <ebates1794@hotmail.com>
2016-04-08 17:05:28 +0000

    home.php now displays fully editable table, TESTER.php exists as a temp php file for buggy code

--- TESTER.php
+++ TESTER.php
@@ -10,13 +10,7 @@
         mysql_select_db("c9",$con);
         
         if(isset($_POST['update'])){
-            $UpdateQuery = "UPDATE `TABLE 2` SET LastName='$_POST[LastName]', FirstName='$_POST[FirstName]', MAT251CalcI = '$_POST[MAT251CalcI]', MAT252CalcII = '$_POST[MAT252CalcII]'
-            , MAT320DiscreteMath = '$_POST[MAT320DiscreteMath]', CPS210CompSciI = '$_POST[CPS210CompSciI]', CPS310CompSciII = '$_POST[CPS310CompSciII]'
-            , CPS315CompSciIII = '$_POST[CPS315CompSciIII]', CPS352OOP = '$_POST[CPS352OOP]', CPS330AssemblyArch = '$_POST[CPS330AssemblyArch]'
-            , CPS353SoftEng = '$_POST[CPS353SoftEng]', CPS415DiscContAlgorithms = '$_POST[CPS415DiscContAlgorithms]', CPS340OpSys = '$_POST[CPS340OpSys]'
-            , CPS425LangProcessing = '$_POST[CPS425LangProcessing]', CPS493Elect1 = '$_POST[CPS493Elect1]', CPS493Projects = '$_POST[CPS493Projects]'
-            , EGC230DigLogic = '$_POST[EGC230DigLogic]', EGC208DigLogicLab = '$_POST[EGC208DigLogicLab]', SCIENCEI = '$_POST[SCIENCEI]'
-            , SCIENCEII = '$_POST[SCIENCEII]' WHERE LastName='$_POST[hidden]'";
+            $UpdateQuery = "UPDATE `TABLE 2` SET LastName='$_POST[LastName]', FirstName='$_POST[FirstName]',WHERE LastName='$_POST[hidden]'";
             mysql_query($UpdateQuery,$con);
             
         };
@@ -25,39 +19,14 @@
             mysql_query($DeleteQuery,$con);
         };
         if(isset($_POST['add'])){
-            $AddQuery = "INSERT INTO `TABLE 2` (LastName, FirstName, MAT251CalcI , MAT252CalcII , MAT320DiscreteMath , CPS210CompSciI, CPS310CompSciII 
-            , CPS315CompSciIII, CPS352OOP, CPS330AssemblyArch, CPS353SoftEng, CPS415DiscContAlgorithms, CPS340OpSys, CPS425LangProcessing, CPS493Elect1
-            , CPS493Projects, EGC230DigLogic, EGC208DigLogicLab, SCIENCEI, SCIENCEII
-            ) 
-            
-            
-            VALUES( '$_POST[uLastName]'
-            ,'$_POST[uFirstName]'
-            ,'$_POST[uMAT251CalcI]'
-            ,'$_POST[uMAT251CalcII]'
-            ,'$_POST[uMAT320DiscreteMath]'
-            ,'$_POST[uCPS210CompSciI]'
-            ,'$_POST[uCPS310CompSciII]'
-            ,'$_POST[uCPS315CompSciIII]'
-            ,'$_POST[uCPS352OOP]'
-            ,'$_POST[uCPS330AssemblyArch]'
-            ,'$_POST[uCPS353SoftEng]'
-            ,'$_POST[uCPS415DiscContAlgorithms]'
-            ,'$_POST[uCPS340OpSys]'
-            ,'$_POST[uCPS425LangProcessing]'
-            ,'$_POST[uCPS493Elect1]'
-            ,'$_POST[uCPS493Projects]'
-            ,'$_POST[uEGC230DigLogic]'
-            ,'$_POST[uEGC208DigLogicLab]'
-            ,'$_POST[uSCIENCEI]'
-            ,'$_POST[uSCIENCEII]')";
+            $AddQuery = "INSERT INTO `TABLE 2` (LastName, FirstName) VALUES( '$_POST[uLastName]','$_POST[uFirstName]')";
             mysql_query($AddQuery,$con);
             
         };
         
         $sql= "SELECT * FROM `TABLE 2`";
         $myData=mysql_query($sql,$con);
-        echo "<table border =1>
+        echo "<table border =2>
         <tr>
         <th>LastName</th>
         <th>FirstName</th>
@@ -84,10 +53,10 @@
         while($record = mysql_fetch_array($myData)){
             echo "<form action=home.php method = post>";
             echo "<tr>";
-            echo "<td>" . "<input type=text name=LastName value=" . $record['LastName'] . "> </td>";
-            echo "<td>" . "<input type=text name=FirstName value=" . $record['FirstName'] . "> </td>";
+            echo "<td>" . "<input type=text name=LastName value=LastName" . $record['LastName'] . "> </td>";
+            echo "<td>" . "<input type=text name=FirstName value=FirstName" . $record['FirstName'] . "> </td>";
             
-            
+            /*
             echo "<td>" . "<input type=text name=MAT251CalcI  value=" . $record['MAT251CalcI'] . "> </td>";
             echo "<td>" . "<input type=text name=MAT252CalcII  value=" . $record['MAT252CalcII'] . "> </td>";
             echo "<td>" . "<input type=text name=MAT320DiscreteMath value=" . $record['MAT320DiscreteMath'] . "> </td>";
@@ -105,8 +74,8 @@
             echo "<td>" . "<input type=text name=EGC230DigLogic value=" . $record['EGC230DigLogic'] . "> </td>";
             echo "<td>" . "<input type=text name=EGC208DigLogicLab value=" . $record['EGC208DigLogicLab'] . "> </td>";
             echo "<td>" . "<input type=text name=SCIENCEI value=" . $record['SCIENCEI'] . "> </td>";
-            echo "<td>" . "<input type=text name=SCIENCEII value=" . $record['SCIENCEII'] . "> </td>";
-            
+            echo "<td>" . "<input type=text name=SCIENCEIIvalue=" . $record['SCIENCEII'] . "> </td>";
+            */
             
             
             echo "<td>" . "<input type=hidden name=hidden value=" .$record['LastName'] . "> </td>";
@@ -121,26 +90,6 @@
         echo "<tr>";
         echo "<td><input type=text name=uLastName></td>";
         echo "<td><input type=text name=uFirstName></td>";
-        
-        echo "<td><input type=text name=uMAT251CalcI></td>";
-        echo "<td><input type=text name=uMAT251CalcII></td>";
-        echo "<td><input type=text name=uMAT320DiscreteMath></td>";
-        echo "<td><input type=text name=uCPS210CompSciI></td>";
-        echo "<td><input type=text name=uCPS310CompSciII></td>";
-        echo "<td><input type=text name=uCPS315CompSciIII></td>";
-        echo "<td><input type=text name=uCPS352OOP></td>";
-        echo "<td><input type=text name=uCPS330AssemblyArch></td>";
-        echo "<td><input type=text name=uCPS353SoftEng></td>";
-        echo "<td><input type=text name=uCPS415DiscContAlgorithms></td>";
-        echo "<td><input type=text name=uCPS340OpSys></td>";
-        echo "<td><input type=text name=uCPS425LangProcessing></td>";
-        echo "<td><input type=text name=uCPS493Elect1></td>";
-        echo "<td><input type=text name=uCPS493Projects></td>";
-        echo "<td><input type=text name=uEGC230DigLogic></td>";
-        echo "<td><input type=text name=uEGC208DigLogicLab></td>";
-        echo "<td><input type=text name=uSCIENCEI></td>";
-        echo "<td><input type=text name=uSCIENCEII></td>";
-        
         echo "<td>" . "<input type=submit name=add value=add>". "</td>";
         echo "</form>";
         
