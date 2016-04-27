ad4d59928672e4c0b130cd2518ec407151e9df0d
seniorgato <ebates1794@hotmail.com>
2016-04-27 15:16:07 +0000

    Saving progress before major step in overall website

--- V2/studentList.php
+++ V2/studentList.php
@@ -1,12 +1,14 @@
 <!DOCTYPE>
 <html>
     <head>
-        <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-   <link rel="stylesheet" href="studentList.css">
+          <!-- Latest compiled and minified CSS -->
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+          <link rel="stylesheet" href="studentList.css">
     </head>
+    
    <body>
-              <div class="container">
+       
+        <div class="container">
             <div class="jumbotron container-fluid">
                 <div class="row">
                    
