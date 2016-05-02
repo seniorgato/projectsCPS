0b302d8da8586d9560e6ee83c830e9a70ca8b0a1
seniorgato <ebates1794@hotmail.com>
2016-05-02 18:14:50 +0000

    Added Tree Diagram of Classes!

--- V2/graphic.php
+++ V2/graphic.php
@@ -6,6 +6,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <script src="go-debug.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gojs/1.6.2/go-debug.js"></script>
         <title>Graphic</title>
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
@@ -13,81 +15,73 @@
     </head>
     
     <body>
-        <div class="container">
-            <div class="container-fluid">
-                <div class="row">
-                   
-                    <div class="col-md-5">
-                        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-                            <div class="container">
-                                <!-- Brand and toggle get grouped for better mobile display -->
-                                <div class="navbar-header">
-                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                                        <span class="sr-only">Toggle navigation</span>
-                                        <span class="icon-bar"></span>
-                                        <span class="icon-bar"></span>
-                                        <span class="icon-bar"></span>
-                                    </button>
-                                    <a class="navbar-brand" href="home.php">
-                                        Student Webapp
-                                    </a>
-                                </div>
-                                <!-- Collect the nav links, forms, and other content for toggling -->
-                                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                                    <ul class="nav navbar-nav">
-                                        <li>
-                                            <a href="home.php">Home</a>
-                                        </li>
-                                        <li>
-                                            <a href="csvUploader.php">CSV Uploader</a>
-                                        </li>
-                                        <li>
-                                            <a href="studentList.php">Student List</a>
-                                        </li>
-                                        
-                                    </ul>
-                                </div>
-                                <!-- /.navbar-collapse -->
-                            </div>
-                            <!-- /.container -->
-                        </nav>
+       <div class="content">
+                  <figure class="org-chart cf">
+                    <ul class="administration">
+                      <li>					
+                        <ul class="director">
+                          <li>
+                            <a href="#"><span>CPS I</span></a>
+                             <ul class="subdirector">
+                      <li><a href="studentList.php"><span>Go Back</span></a></li>
+                            </ul>
+                            <ul class="departments cf">								
+                              <li><a href="#"><span>CPS II</span></a></li>
+                              
+                              
+                              <li class="department dep-a">
+                                <a href="#"><span>Knowledge Part 1</span></a>
+                                <ul class="sections">
+                                  <li class="section"><a href="#"><span>Assembly & Architecture</span></a></li>
+                                  <li class="section"><a href="#"><span>CS-3</span></a></li>
+                                  <li class="section"><a href="#"><span>Object Oriented Programming</span></a></li>
+                                  <li class="section"><a href="#"><span>Discrete Math for CS</span></a></li>
+                                </ul>
+                              </li>
+                              <li class="department dep-b">
+                                <a href="#"><span>Knowledge Part 2</span></a>
+                                <ul class="sections">
+                                  <li class="section"><a href="#"><span>Operating Systems</span></a></li>
+                                  <li class="section"><a href="#"><span>Language Processing</span></a></li>
+                                  <li class="section"><a href="#"><span>Software Engineering</span></a></li>
+                                  <li class="section"><a href="#"><span>Discrete & Continuous Algorithms</span></a></li>
 
-                    </div>
+                                </ul>
+                              </li>
+                              <li class="department dep-c">
+                                <a href="#"><span>Math, Science, & Engineering</span></a>
+                                <ul class="sections">
+                                  <li class="section"><a href="#"><span>Calculus I</span></a></li>
+                                  <li class="section"><a href="#"><span>Calculus II</span></a></li>
+                                  <li class="section"><a href="#"><span>Science I</span></a></li>
+                                  <li class="section"><a href="#"><span>Science II</span></a></li>
+                                  <li class="section"><a href="#"><span>Digital Logic Design</span></a></li>
+                                  <li class="section"><a href="#"><span>Digital Logic Lab</span></a></li>
+                                </ul>
+                              </li>
+                              <li class="department dep-d">
+                                <a href="#"><span>Skills (Electives)</span></a>
+                                <ul class="sections">
+                                  <li class="section"><a href="#"><span>Database</span></a></li>
+                                  <li class="section"><a href="#"><span>Web Programming</span></a></li>
+                                  <li class="section"><a href="#"><span>Networks</span></a></li>
+                                  <li class="section"><a href="#"><span>AI/Robotics</span></a></li>
+                                  <li class="section"><a href="#"><span>Embedded Linux</span></a></li>
+                                </ul>
+                              </li>
+                              <li class="department dep-e">
+                                <a href="#"><span>Projects CPS</span></a>
+                                <ul class="sections">
+                                </ul>
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>	
+                      </li>
+                    </ul>			
+                  </figure>
                 </div>
-                
-               
-               
-                <div>
-                      
-                                                   
-                </div>
-                
-                
-                
-                
-          
-            </div>
-
-            
-         
-           
-            
-            
-            
-            
-            
-            
-        </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-        <script type="text/javascript">
-            $(".progress-bar").animate({
-                width: "75%"
-            }, 2000);
-        </script>
-        <!-- it works the same with all jquery version from 1.x to 2.x -->
-        <script src="jquery.min.js"></script>
-        <script src="jssor.slider.mini.js"></script>
+       
     </body>
 
 </html>
